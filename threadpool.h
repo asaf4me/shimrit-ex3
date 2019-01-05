1d21e463018ab8e0f96e3b30eebd9151ccc3d3a1
asaf4me <asaf4me@gmail.com>
2019-01-05 15:23:06 +0200

    threadpool.c

--- threadpool.h
+++ threadpool.h
@@ -1,3 +1,5 @@
+#if !defined(THREADPOOL_H)
+#define THREADPOOL_H
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,42 +17,41 @@
 
 typedef enum
 {
-    false,
-    true
+	false,
+	true
 } bool;
-
 
 /**
  * the pool holds a queue of this structure
  */
-typedef struct work_st{
-      int (*routine) (void*);  //the threads process function
-      void * arg;  //argument to the function
-      struct work_st* next;  
+typedef struct work_st
+{
+	int (*routine)(void *); //the threads process function
+	void *arg;				//argument to the function
+	struct work_st *next;
 } work_t;
-
 
 /**
  * The actual pool
  */
-typedef struct _threadpool_st {
- 	int num_threads;	//number of active threads
-	int qsize;	        //number in the queue
-	pthread_t *threads;	//pointer to threads
-	work_t* qhead;		//queue head pointer
-	work_t* qtail;		//queue tail pointer
+typedef struct _threadpool_st
+{
+	int num_threads;			//number of active threads
+	int qsize;					//number in the queue
+	pthread_t *threads;			//pointer to threads
+	work_t *qhead;				//queue head pointer
+	work_t *qtail;				//queue tail pointer
 	pthread_mutex_t qlock;		//lock on the queue list
-	pthread_cond_t q_not_empty;	//non empty and empty condidtion vairiables
+	pthread_cond_t q_not_empty; //non empty and empty condidtion vairiables
 	pthread_cond_t q_empty;
-      int shutdown;            //1 if the pool is in distruction process     
-      int dont_accept;       //1 if destroy function has begun
+	int shutdown;	//1 if the pool is in distruction process
+	int dont_accept; //1 if destroy function has begun
 } threadpool;
-
 
 // "dispatch_fn" declares a typed function pointer.  A
 // variable of type "dispatch_fn" points to a function
 // with the following signature:
-// 
+//
 //     int dispatch_function(void *arg);
 
 typedef int (*dispatch_fn)(void *);
@@ -65,8 +66,7 @@ typedef int (*dispatch_fn)(void *);
  * 3. initialized mutex and conditional variables
  * 4. create the threads, the thread init function is do_work and its argument is the initialized threadpool. 
  */
-threadpool* create_threadpool(int num_threads_in_pool);
-
+threadpool *create_threadpool(int num_threads_in_pool);
 
 /**
  * dispatch enter a "job" of type work_t into the queue.
@@ -79,7 +79,7 @@ threadpool* create_threadpool(int num_threads_in_pool);
  * 4. unlock mutex
  *
  */
-void dispatch(threadpool* from_me, dispatch_fn dispatch_to_here, void *arg);
+void dispatch(threadpool *from_me, dispatch_fn dispatch_to_here, void *arg);
 
 /**
  * The work function of the thread
@@ -91,14 +91,13 @@ void dispatch(threadpool* from_me, dispatch_fn dispatch_to_here, void *arg);
  * 5. call the thread routine
  *
  */
-void* do_work(void* p);
-
+void *do_work(void *p);
 
 /**
  * destroy_threadpool kills the threadpool, causing
  * all threads in it to commit suicide, and then
  * frees all the memory associated with the threadpool.
  */
-void destroy_threadpool(threadpool* destroyme);
+void destroy_threadpool(threadpool *destroyme);
 
-
+#endif
