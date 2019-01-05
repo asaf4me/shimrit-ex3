1d3a4f8ff40b05ecb57a231a1bde213567bd8448
asaf4me <asaf4me@gmail.com>
2019-01-05 15:23:45 +0200

    threadpool.c

--- threadpool.c
+++ threadpool.c
@@ -1,6 +1,7 @@
 #include "threadpool.h"
 
-void usage(){
+void usage()
+{
     printf("Usage: server <port> <pool-size> <max-number-of-request>");
 }
 
@@ -19,6 +20,20 @@ threadpool *create_threadpool(int num_threads_in_pool)
     }
     threadpool->num_threads = num_threads_in_pool;
     threadpool->qsize = 0;
+    threadpool->threads = (pthread_t *)malloc(threadpool->num_threads * sizeof(pthread_t));
+    if (threadpool == NULL)
+    {
+        perror("malloc");
+        return NULL;
+    }
+    for (int i = 0; i < threadpool->num_threads; i++)
+    {
+        if (pthread_create(&threadpool->threads[i], NULL, do_work, (void *)dispatch))
+        {
+            printf("ERROR\n");
+            exit(-1);
+        }
+    }
     threadpool->qhead = NULL;
     threadpool->qtail = NULL;
     // threadpool->qlock =
@@ -28,4 +43,16 @@ threadpool *create_threadpool(int num_threads_in_pool)
     threadpool->dont_accept = 0;
 
     return threadpool;
+}
+
+void dispatch(threadpool *from_me, dispatch_fn dispatch_to_here, void *arg)
+{
+}
+
+void *do_work(void *p)
+{
+}
+
+void destroy_threadpool(threadpool *destroyme)
+{
 }
